b0ba825f2a789c65812531f20cb55405c831d3b6
UserTest <User123Test321@mail.ru>
2018-07-06 07:22:32 +0300

    1

--- src/test/java/test1/pages/GoogleTranslatePage.java
+++ src/test/java/test1/pages/GoogleTranslatePage.java
@@ -1,22 +1,18 @@
 package test1.pages;
 
-import net.thucydides.core.annotations.DefaultUrl;
-import net.serenitybdd.core.pages.WebElementFacade;
-
 import net.serenitybdd.core.annotations.findby.FindBy;
-
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
 
 @DefaultUrl("https://translate.google.com/")
 public class GoogleTranslatePage extends PageObject {
 
-
     @FindBy(id="source")
     private WebElementFacade translateTerms;
 
-    @FindBy(id="gt-res-c")
+    @FindBy(xpath = "//*[@id=result_box]/span")
     private WebElementFacade resultField;
-
 
     public void enter_keywords(String keyword) {
         translateTerms.sendKeys(keyword);
